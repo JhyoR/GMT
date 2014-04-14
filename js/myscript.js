8c088e67ae9e56b155a47010bdb3a7ecc40bdd58
JhyoR <jhyoramona@gmail.com>
2014-04-14 14:53:44 +0700

    13231

--- js/myscript.js
+++ js/myscript.js
@@ -1,28 +1,11 @@
 $(document).ready(function() {
-event.preventDefault();
 
 $( ".setting_panel" ).click(function() {
 $( this ).toggleClass( "setting_panel_open" );
 });
 
-
-$( ".setting_panel .selectv-1" ).click(function() {
-$( ".element-2" ).addClass( "selectedd" );
-$( ".element-3" ).removeClass( "selectedd" );
+$( 'a.selectv' ).click( function(){
+  $( this ).addClass( 'selectedd' );
 });
-$( ".setting_panel .selectv-2" ).click(function() {
-$( ".element-3" ).addClass( "selectedd" );
-$( ".element-2" ).removeClass( "selectedd" );
-});
-
-$( ".body .selectv-1" ).click(function() {
-$( ".login_box.v2" ).addClass( "hidee" );
-$( ".login_box.v1" ).removeClass( "hidee" );
-});
-$( ".body .selectv-2" ).click(function() {
-$( ".login_box.v1" ).addClass( "hidee" );
-$( ".login_box.v2" ).removeClass( "hidee" );
-});
-
 
 });
